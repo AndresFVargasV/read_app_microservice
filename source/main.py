4a5c297b3e126d0ced1b45f1ffac2a3b45759780
AndresFVargasV <afvasquez@uninorte.edu.co>
2023-11-10 20:50:09 -0500

    Test version

--- source/main.py
+++ source/main.py
@@ -1,4 +1,7 @@
 from flask import Flask, request, jsonify
+import json
+from datetime import date
+import base64 
 import dbconfig as db
 
 
@@ -16,10 +19,25 @@ def get():
         return jsonify({'msg': 'No se pudo conectar a la base de datos'})
     cursor = conn.cursor() 
     cursor.execute('SELECT * FROM Personas')
-    users = cursor.fetchall()
+    data = cursor.fetchall()
+    column_names = [column[0] for column in cursor.description]
+    results = []
+    
+    for row in data:
+        result_row = {}
+        for i, value in enumerate(row):
+            if isinstance(value, date):
+                result_row[column_names[i]] = value.strftime('%Y-%m-%d')
+            elif isinstance(value, bytes): 
+                result_row[column_names[i]] = base64.b64encode(value).decode() 
+            else:
+                result_row[column_names[i]] = value
+        results.append(result_row)
+
+    json_data = json.dumps(results)
     cursor.close()
     conn.close()
-    return jsonify(users)
+    return jsonify(json_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
