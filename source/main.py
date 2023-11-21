8bc76c82aa21557c3e09c0e9d37195c53a967ed8
Bryamv <bryamv@uninorte.edu.co>
2023-11-21 15:47:55 -0500

    log route added

--- source/main.py
+++ source/main.py
@@ -85,6 +85,41 @@ def get_one(id):
         return make_response(jsonify({'error': 'No se pudo establecer la conexión con MongoDB'}), 500)
 
 
+
+
+# Ruta para obtener todos los registros de la colección "logs"
+@app.route('/api/log', methods=['GET'])
+def get_logs():
+    conexion = dbase.establecer_conexion()
+
+    # Verifica si la conexión fue exitosa
+    if conexion:
+        
+        # Selecciona la base de datos y la colección "logs"
+        db, collection = dbase.seleccionar_bd_y_coleccion(conexion, "crud", "logs")
+
+        # Busca todos los registros en la colección "logs"
+        registros = collection.find()
+
+        # Lista para almacenar la información de los registros con id de mongodb convertidos a string
+        registros_id_string = []
+
+        # Iteramos cada registro para convertir el id de mongodb a string
+        for registro in registros:
+            registro['_id'] = str(registro['_id'])
+            registros_id_string.append(registro)
+
+        # Cierra la conexión al finalizar
+        dbase.cerrar_conexion(conexion)
+
+        # Devuelve la respuesta JSON después de la iteración
+        return Response(json.dumps({'registros': registros_id_string}, default=json_util.default), mimetype='application/json')
+    else:
+
+        # Devuelve la respuesta JSON teniendo en cuenta que la conexión no fue exitosa
+        return make_response(jsonify({'error': 'No se pudo establecer la conexión con MongoDB'}), 500)
+
+
 # Inicializa la aplicación Flask desde este modeulo base
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
